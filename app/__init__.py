12821d8cb0cc3e2fd25df02eb084580a3fae4e18
KosAqH <marcin_wlodarczyk@protonmail.com>
2024-01-06 11:14:01 +0100

    Login system took from my other app.
    https://github.com/KosAqH/SimpleToDoApp

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+app = Flask(__name__, instance_relative_config=False)
+
+app.config['SECRET_KEY'] = 'this-shouldnt-be-here-in-real-app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+from .models import User
+
+# Register blueprints
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+with app.app_context():
+    db.create_all()
